8e9269532d698d669bdccdf440f05489ad6eb1a6
AlvaroITESO <alvaro.nayru@gmail.com>
2019-05-18 00:10:42 -0500

    backend

---
+++ Backend/mongodb/mongodb-connect.js
@@ -0,0 +1,10 @@
+'use strict'
+
+let mongoose= require('mongoose');
+let mongoDB = 'mongodb+srv://is702396_:alvaro1.@cluster0-xftk7.mongodb.net/test?retryWrites=true'
+mongoose.connect(mongoDB,{
+    useNewUrlParser: true,
+    useCreateIndex: true});
+let db = mongoose.connection;
+
+module.exports = {mongoose};
